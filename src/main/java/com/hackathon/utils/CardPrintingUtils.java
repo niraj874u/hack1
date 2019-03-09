3a99d5330eb1ec6148b92da47a074b53e2e750a1
Niraj Patel <niraj874u@gmail.com>
2019-03-09 17:17:30 +0530

    Add Template

--- src/main/java/com/hackathon/utils/CardPrintingUtils.java
+++ src/main/java/com/hackathon/utils/CardPrintingUtils.java
@@ -1,7 +1,11 @@
 package com.hackathon.utils;
 
+import java.io.StringWriter;
 import java.util.List;
 
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,10 +20,27 @@ public class CardPrintingUtils {
 	public static void printCards(List<IDCardDTO> cards , String tenplatePath, String outputFilePath)
 	{
 		logger.info("filePath = "+outputFilePath);
+		
+		/*  first, get and initialize an engine  */
+        VelocityEngine ve = new VelocityEngine();
+        ve.init();
+        /*  next, get the Template  */
+        Template t = ve.getTemplate("src/main/resources/template.vm" );
+        /*  create a context and add data */
+        VelocityContext context = new VelocityContext();
+        
+        context.put("cards", cards);
+        /* now render the template into a StringWriter */
+        StringWriter writer = new StringWriter();
+        t.merge( context, writer );
+        /* show the World */
+        System.out.println( writer.toString() );   
+        
                 try
                 {
                 	for(IDCardDTO card : cards)
                 	{
+                		/*
                 		System.out.println("----------------------------------");
                 		System.out.println("Event Name :"  + card.eventName);
                 		System.out.println("Host Logo  :"  + card.logoPath);
@@ -35,8 +56,7 @@ public class CardPrintingUtils {
                 		System.out.println("Group Name :"  + card.grouopName);
                 		System.out.println("Icon Path :"  + card.iconPath);
                 		System.out.println("Barcode Path :"  + card.barcodePath);
-                		
-                		
+                		*/
                 	}
                 }
                 catch (Exception e) {
