4acf3530b90e178f414235e4647ec2e79bb49fc6
Niraj Patel <niraj874u@gmail.com>
2019-03-09 08:45:47 +0530

    Init Commit

---
+++ src/test/java/com/hackathon/TC1.java
@@ -0,0 +1,15 @@
+package com.hackathon;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TC1 {
+
+	@Test
+	void test() {
+		System.out.println("Hello");
+		fail("Not yet implemented");
+	}
+
+}
